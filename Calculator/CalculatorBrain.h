edace488b590e8602c19c3a6846e963d10677806
Kristen Sundquist <kristen@csc8.com>
2012-07-11 21:38:27 -0700

    added CalculatorBrain files

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Kristen Sundquist on 7/11/12.
+//  Copyright (c) 2012 Me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Kristen Sundquist on 7/11/12.
+//  Copyright (c) 2012 Me. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end
